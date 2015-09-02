6d60bb6be7885e42d9d0260d565204c03da4bc62
Sean <lettersforsean@yahoo.co.jp>
2015-09-02 11:58:34 +1000

    Configure spec_helper to use webmock.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pv_output_wrapper'
-require 'pv_output_wrapper/query'
+require 'pv_output_wrapper/request'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  # config.filter_gems_from_backtrace "ignored_gem", "another_ignored_gem"
+end
