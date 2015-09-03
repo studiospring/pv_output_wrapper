8a6976ca67e4f5147fe554b317d8322ed63dae40
Sean <lettersforsean@yahoo.co.jp>
2015-09-03 10:54:44 +1000

    Refactor Request.

--- lib/pv_output_wrapper/request.rb
+++ lib/pv_output_wrapper/request.rb
@@ -15,37 +15,12 @@ module PvOutputWrapper
         'X-Pvoutput-Apikey' => api_key,
         'X-Pvoutput-Systemid' => system_id
       }
-      @connection = Net::HTTP.new(HOST, 80)
-    end
-
-    # @param [String, URI] full uri including any params.
-    def get_request(uri)
-      Net::HTTP::Get.new(uri)
-    end
-
-    # @param [String] service path as defined by pvoutput.org.
-    def service_path(service)
-      uri_string = "#{HOST}/service/#{revision}/#{service}.jsp/{?query*}"
-      Addressable::URI.parse(uri_string)
-    end
-
-    # @return [String] the api revision number.
-    def revision(rev: 'r2')
-      rev
-    end
-
-    # @params [String] service name, [Hash] query params.
-    # @return [Addressable::URI] pvoutput uri.
-    def construct_uri(service, params={})
-      template = Addressable::Template.new(service_path(service))
-      template.expand({"query" => params})
     end
 
     # @return [Hash<String>] system data or nil values upon failure.
     def get_statistic(params={})
       uri = construct_uri('getstatistic', params)
-      response = @connection.get(uri, @headers)
-      response.body
+      get_response(uri).body
 
       # total_output is in watt hours
       # keys = ['total_output', 'efficiency', 'date_from', 'date_to']
@@ -53,5 +28,31 @@ module PvOutputWrapper
       # selected_results = results_array.values_at(0, 5, 7, 8)
       # Hash[keys.zip selected_results]
     end
+
+    private
+
+      # @param [String, URI] full uri including any params.
+      def get_response(uri)
+        connection = Net::HTTP.new(HOST, 80)
+        connection.get(uri, @headers)
+      end
+
+      # @params [String] service name, [Hash] query params.
+      # @return [Addressable::URI] pvoutput uri.
+      def construct_uri(service, params={})
+        template = Addressable::Template.new(service_path(service))
+        template.expand({"query" => params})
+      end
+
+      # @param [String] service path as defined by pvoutput.org.
+      def service_path(service)
+        uri_string = "#{HOST}/service/#{revision}/#{service}.jsp/{?query*}"
+        Addressable::URI.parse(uri_string)
+      end
+
+      # @return [String] the api revision number.
+      def revision(rev: 'r2')
+        rev
+      end
   end
 end
