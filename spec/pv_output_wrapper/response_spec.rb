967afaf0d231ddfc9e90988986260315887a9217
Sean <lettersforsean@yahoo.co.jp>
2015-09-15 13:23:14 +1000

    Response spec returns symbols for keys.

--- spec/pv_output_wrapper/response_spec.rb
+++ spec/pv_output_wrapper/response_spec.rb
@@ -26,11 +26,11 @@ describe PvOutputWrapper::Response do
 
     it 'should return a hash' do
       system_stats = {
-        'total_output' => '246800',
-        'efficiency' => '3.358',
-        'date_from' => '20100901',
-        'date_to' => '20100927'
-        }
+        :total_output => '246800',
+        :efficiency => '3.358',
+        :date_from => '20100901',
+        :date_to => '20100927'
+      }
 
       expect(@parsed_response).to eq(system_stats)
     end
