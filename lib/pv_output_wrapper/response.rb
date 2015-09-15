4a53742103f7cc2cbaef8d6925e6ee5d39e80036
Sean <lettersforsean@yahoo.co.jp>
2015-09-15 12:39:11 +1000

    Refactor get_statistic parsing.

--- lib/pv_output_wrapper/response.rb
+++ lib/pv_output_wrapper/response.rb
@@ -21,13 +21,12 @@ module PvOutputWrapper
 
     private
 
-      # @return [Hash<String, >] system data or nil values upon failure.
-      def get_statistic
-        # total_output is in watt hours
-        keys = ['total_output', 'efficiency', 'date_from', 'date_to']
-        results_array = body.split(/,/)
-        selected_results = results_array.values_at(0, 5, 7, 8)
-        Hash[keys.zip selected_results]
-      end
+    # @return [Hash<String, >] system data or nil values upon failure.
+    # :total_output is in watt hours.
+    def get_statistic
+      keys = [:total_output, :efficiency, :date_from, :date_to]
+      selected_results = body.split(/,/).values_at(0, 5, 7, 8)
+      Hash[keys.zip selected_results]
+    end
   end
 end
