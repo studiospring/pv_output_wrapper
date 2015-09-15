a6c784233c4ce0c25d5c604c6bf4ef9c34d6a60d
Sean <lettersforsean@yahoo.co.jp>
2015-09-15 12:40:00 +1000

    Remove useless spec.

--- spec/pv_output_wrapper/pv_output_wrapper_spec.rb
+++ spec/pv_output_wrapper/pv_output_wrapper_spec.rb
@@ -4,8 +4,4 @@ describe PvOutputWrapper do
   it 'has a version number' do
     expect(PvOutputWrapper::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
