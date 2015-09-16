58546e967e1ef21dfb52ed8d87732761bc2d8d3b
Sean <lettersforsean@yahoo.co.jp>
2015-09-16 22:07:33 +1000

    Add 'search' service.

--- lib/pv_output_wrapper.rb
+++ lib/pv_output_wrapper.rb
@@ -8,7 +8,8 @@ module PvOutputWrapper
   # Service names must be the same as corresponding pvoutput.org api path name,
   #   with optional underscores.
   VALID_SERVICES = {:get_statistic => [:df, :dt, :c, :crdr, :sid],
-                    :get_status => [:d, :t, :h, :asc, :limit, :from, :to, :ext, :sid1]}
+                    :get_status => [:d, :t, :h, :asc, :limit, :from, :to, :ext, :sid1],
+                    :search => [:q, :ll, :country]}
 
   class Logger
     attr_accessor :logger
