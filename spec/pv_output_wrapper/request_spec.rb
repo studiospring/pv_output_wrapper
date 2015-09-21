9a7058e867697996c4f512a08d004a94cb11dce7
Sean <lettersforsean@yahoo.co.jp>
2015-09-21 16:43:06 +1000

    Use PvoStub to create stub_request.

--- spec/pv_output_wrapper/request_spec.rb
+++ spec/pv_output_wrapper/request_spec.rb
@@ -2,54 +2,35 @@ require "spec_helper"
 require "addressable/uri"
 
 describe PvOutputWrapper::Request do
+  let(:expected_body) { "246800,246800,8226,2000,11400,3.358,27,20100901,20100927,4.653,20100916" }
   before do
     # Malformed uri is to correct a Webmock/Addressable bug.
-    @base_uri = "http://http//www.pvoutput.org:80/service/r2/"
     @request = PvOutputWrapper::Request.new('my_api_key', 'my_system_id')
-    @response_body = "246800,246800,8226,2000,11400,3.358,27,20100901,20100927,4.653,20100916"
-    @headers = {'Accept' => '*/*',
-                'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                'User-Agent' => 'Ruby',
-                'X-Pvoutput-Apikey' => 'my_api_key',
-                'X-Pvoutput-Systemid' => 'my_system_id'
-              }
   end
 
   describe 'method_missing' do
     context 'when no params are entered' do
-      before do
-        uri = @base_uri + "getstatistic.jsp/"
-        stub_request(:get, uri)
-          .with(:headers => @headers)
-          .to_return(:status => 200, :body => @response_body)
+      let(:expected_body) do
+        "246800,246800,8226,2000,11400,3.358,27,20100901,20100927,4.653,20100916"
       end
 
+      let!(:stub) { PvoStub.new('getstatistic', expected_body) }
+
       it "should return the correct response body." do
-        expect(@request.get_statistic.body).to eq(@response_body)
+        expect(@request.get_statistic.body).to eq(expected_body)
       end
     end
 
     context 'when some params are entered' do
-      before do
-        uri = @base_uri + "getstatistic.jsp/?df=20150101"
-        stub_request(:get, uri)
-          .with(:headers => @headers)
-          .to_return(:status => 200, :body => @response_body)
-      end
+      let(:query) { {:df => '20150101'} }
+      let!(:stub) { PvoStub.new('getstatistic', expected_body, query) }
 
       it "should return the correct response body." do
-        expect(@request.get_statistic(:df => '20150101').body).to eq(@response_body)
+        expect(@request.get_statistic(:df => '20150101').body).to eq(expected_body)
       end
     end
 
     context 'when an unrecognised service is requested' do
-      before do
-        uri = @base_uri + "foobar.jsp/"
-        stub_request(:get, uri)
-          .with(:headers => @headers)
-          .to_return(:status => 200, :body => @response_body)
-      end
-
       it 'should raise a NoMethodError error' do
         expect { @request.foobar }.to raise_error(NoMethodError)
       end
@@ -62,12 +43,8 @@ describe PvOutputWrapper::Request do
     end
 
     context 'when invalid params are entered' do
-      before do
-        uri = @base_uri + "getstatistic.jsp/?foo=bar"
-        stub_request(:get, uri)
-          .with(:headers => @headers)
-          .to_return(:status => 200, :body => @response_body)
-      end
+      let(:query) { {:foo => 'bar'} }
+      let!(:stub) { PvoStub.new('getstatistic', expected_body, query) }
 
       it 'should raise an error' do
         expect { @request.get_statistic(:foo => 'bar') }.to raise_error(ArgumentError)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,10 @@ require 'pv_output_wrapper'
 require 'pv_output_wrapper/request'
 require 'pv_output_wrapper/response'
 require 'webmock/rspec'
+require 'support/utilities'
 
 WebMock.disable_net_connect!(:allow_localhost => true)
 
-RSpec.configure do |config|
+RSpec.configure do |_config|
   # config.filter_gems_from_backtrace "ignored_gem", "another_ignored_gem"
 end
