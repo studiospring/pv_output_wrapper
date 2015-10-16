d3f249026af8caa5f7e987bff0449adc0e4785bf
Sean <lettersforsean@yahoo.co.jp>
2015-10-16 11:18:48 +1100

    Fix pvoutput hostname again.

--- lib/pv_output_wrapper.rb
+++ lib/pv_output_wrapper.rb
@@ -5,7 +5,8 @@ require "pv_output_wrapper/response"
 module PvOutputWrapper
   # Include the scheme to prevent Addressable bug.
   # See {https://github.com/bblimke/webmock/issues/489a}
-  HOST = 'http://pvoutput.org'
+  # NO! DON'T! Or else you get a SocketError.
+  HOST = 'pvoutput.org'
 
   # Service names must be the same as corresponding pvoutput.org api path name,
   #   with optional underscores.
